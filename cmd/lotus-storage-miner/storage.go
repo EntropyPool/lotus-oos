3fe130e1cb9d795a3bd200f4c13dcd9268129152
Zhao KK <kikakkz@hotmail.com>
2021-03-09 15:48:33 +0800

    Use full miner address as file object prefix

--- cmd/lotus-storage-miner/storage.go
+++ cmd/lotus-storage-miner/storage.go
@@ -155,20 +155,13 @@ over time
 					return err
 				}
 
-				mid, err := address.IDFromAddress(ma)
-				if err != nil {
-					return err
-				}
-
-				miner := abi.ActorID(mid)
-
 				cfg.OssInfo = stores.StorageOSSInfo{
 					CanWrite:   cctx.Bool("store"),
 					URL:        cctx.String("oss-url"),
 					AccessKey:  cctx.String("oss-access-key"),
 					SecretKey:  cctx.String("oss-secret-key"),
 					BucketName: cctx.String("oss-bucket-name"),
-					Prefix:     fmt.Sprintf("%v", miner),
+					Prefix:     fmt.Sprintf("%v", ma),
 				}
 			}
 
